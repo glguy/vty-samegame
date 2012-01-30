4af378dd7581d887fdb347f980f11af643b28794
Eric Mertens <emertens@gmail.com>
2012-01-29 17:14:06 -0800

    Add score tracking

--- Main.hs
+++ Main.hs
@@ -11,23 +11,38 @@ import System.Random
 import qualified Data.Set as Set
 import Data.Set (Set)
 
+scoringFunction i = (i - 2) ^ (2 :: Int)
+clearScreenBonus  = 1000
+animationDelay    = 150000
+gameSize          = (9,14)
+
+mkNewGame :: IO GameState
+mkNewGame = do
+  arr <- newBoard gameSize
+  return (mkGameState (0,0) arr Nothing 0)
+
+main :: IO ()
 main = bracket mkVty shutdown $ \vty -> do
   eventVar <- newMVar Refresh
-  arr <- newBoard (10,20)
-  gameVar  <- newIORef $ mkGameState (2,3) arr
+  gameVar  <- newIORef =<< mkNewGame 
   forkIO (clock eventVar)
   forkIO (drawingThread vty eventVar gameVar 0)
   
   let loop = do
         ev <- next_event vty
         case ev of
-          EvKey KUp []    -> transform (moveCursor coordUp)
-          EvKey KDown []  -> transform (moveCursor coordDown)
-          EvKey KLeft []  -> transform (moveCursor coordLeft)
-          EvKey KRight [] -> transform (moveCursor coordRight)
-          EvKey KEsc []   -> return ()
-          EvKey KEnter [] -> transform captureGroup
-          _ -> loop
+          EvKey KUp      [] -> transform (moveCursor coordUp)
+          EvKey KDown    [] -> transform (moveCursor coordDown)
+          EvKey KLeft    [] -> transform (moveCursor coordLeft)
+          EvKey KRight   [] -> transform (moveCursor coordRight)
+          EvKey KEnter   [] -> transform captureGroup
+          EvKey KEsc     [] -> return ()
+          EvKey (KASCII 'n') [] -> restart
+          _                 -> loop
+
+      restart = do
+        g <- mkNewGame
+        transform (\_ -> g)
 
       transform f = do
         modifyIORef gameVar f
@@ -41,47 +56,78 @@ main = bracket mkVty shutdown $ \vty -> do
 type Coord = (Int,Int)
 
 data GameState = GameState
-  { cursorCoord :: Coord
-  , boardArray :: Array Coord (Maybe Piece)
+  { cursorCoord        :: Coord
+  , boardArray         :: Array Coord (Maybe Piece)
   , cachedConnectedSet :: Set Coord
+  , lastScore          :: Maybe Int
+  , totalScore         :: Int
+  , gameOver           :: Bool
   }
 
-mkGameState c a = GameState
-  { cursorCoord = c
-  , boardArray  = a
+mkGameState ::
+  Coord {- ^ current cursor coordinate -}         ->
+  Array Coord (Maybe Piece) {- ^ current board -} ->
+  Maybe Int {- ^ recent score change -}           ->
+  Int {- ^ total score -}                         ->
+  GameState
+mkGameState c a ls ts = GameState
+  { cursorCoord        = c
+  , boardArray         = a
   , cachedConnectedSet = connectedSet c a
+  , lastScore          = ls
+  , totalScore         = ts
+  , gameOver           = isGameOver a
   }
 
+coordUp, coordDown, coordLeft, coordRight :: Coord -> Coord
 coordUp    (r,c) = (r-1,c)
 coordDown  (r,c) = (r+1,c)
 coordLeft  (r,c) = (r,c-1)
 coordRight (r,c) = (r,c+1)
 
+moveCursor :: (Coord -> Coord) -> GameState -> GameState
 moveCursor direction st
-  | rangeTest cursor' = mkGameState cursor' (boardArray st)
+  | rangeTest cursor' = mkGameState cursor' (boardArray st) Nothing (totalScore st)
   | otherwise         = st
   where
-  cursor' = direction (cursorCoord st)
-  rangeTest = inRange (bounds (boardArray st))
+  cursor'   = direction (cursorCoord st)
+  rangeTest = inArray (boardArray st)
 
-moveDown st@GameState { cursorCoord = (r,c), boardArray = arr }
-  | inRange (bounds arr) (r+1,c) = mkGameState (r+1,c) arr
-  | otherwise                    = st
-
-moveLeft st@GameState { cursorCoord = (r,c), boardArray = arr }
-  | inRange (bounds arr) (r,c-1) = st { cursorCoord = (r,c-1) }
-  | otherwise                    = st
-
-moveRight st@GameState { cursorCoord = (r,c), boardArray = arr }
-  | inRange (bounds arr) (r,c+1) = st { cursorCoord = (r,c+1) }
-  | otherwise                    = st
-
+captureGroup :: GameState -> GameState
 captureGroup st
-  | Set.size (cachedConnectedSet st) < 2 = st
-  | otherwise      = mkGameState (cursorCoord st)
-                        (collapseHoles $ boardArray st // [ (c, Nothing) | c <- Set.toList (cachedConnectedSet st) ])
+  | groupSize < 2 = st
+  | otherwise = mkGameState (cursorCoord st) board' (Just score) (totalScore st + score)
+  where
+  groupSize = Set.size (cachedConnectedSet st)
+  board' = collapseHoles (deleteSet (cachedConnectedSet st) (boardArray st))
+  score = scoringFunction groupSize + bonus
 
-arrayToList arr = filter (not . null) $ map (\c -> (mapMaybe (\r -> arr ! (r,c)) [rowHi,rowHi-1..rowLo])) [colLo..colHi]
+  bonus
+    | all isNothing (elems board') = clearScreenBonus
+    | otherwise                    = 0
+
+isGameOver :: Array Coord (Maybe Piece) -> Bool
+isGameOver a = not (any isPaired (assocs a))
+  where
+  isPaired (_,Nothing) = False
+  isPaired (c,Just p)  = any isMatch (neighbors a c)
+    where
+    isMatch x = a ! x == Just p
+
+deleteSet :: Set Coord -> Array Coord (Maybe Piece) -> Array Coord (Maybe Piece)
+deleteSet set arr = arr // [ (c, Nothing) | c <- Set.toList set ]
+
+
+collapseHoles :: Array Coord (Maybe Piece) -> Array Coord (Maybe Piece)
+collapseHoles arr = accumArray (\_ a -> Just a) Nothing (bounds arr)
+                               (toCoords arr (arrayToList arr))
+
+arrayToList :: Array Coord (Maybe Piece) -> [[Piece]]
+arrayToList arr =
+  filter (not . null)     $
+  flip map      [colLo..colHi]         $ \c ->
+  flip mapMaybe [rowHi,rowHi-1..rowLo] $ \r ->
+    arr ! (r,c)
   where
   ((rowLo, colLo),(rowHi,colHi)) = bounds arr
 
@@ -89,14 +135,11 @@ toCoords arr = concat . zipWith (\c -> zipWith (\r x -> ((r,c),x)) [rowHi,rowHi-
   where
   ((rowLo, colLo),(rowHi,colHi)) = bounds arr
 
-collapseHoles arr = accumArray (\_ a -> Just a) Nothing (bounds arr) (toCoords arr (arrayToList arr))
-  where
-  ((rowLo, colLo),(rowHi,colHi)) = bounds arr
-
-
-newBoard sz = liftM (listArray ((0,0),sz) . map Just) (replicateM n randomPiece)
-  where
-  n = (fst sz + 1) * (snd sz + 1)
+newBoard :: Coord -> IO (Array Coord (Maybe Piece))
+newBoard sz = do
+  let n = (fst sz + 1) * (snd sz + 1)
+  xs <- replicateM n randomPiece
+  return (listArray ((0,0),sz) (map Just xs))
 
 connectedSet :: Coord -> Array Coord (Maybe Piece) -> Set Coord
 connectedSet c arr = case arr ! c of
@@ -105,76 +148,119 @@ connectedSet c arr = case arr ! c of
     where
     aux q res = case Set.minView q of
       Nothing -> res
-      Just (x,q') | arr ! x /= Just p  -> aux q' res
-                  | Set.member x res -> aux q' res
-                  | otherwise        -> aux (foldr Set.insert q' (filter (inRange (bounds arr)) (neighbors x))) (Set.insert x res)
+      Just (x,q')
+        | arr ! x /= Just p  -> aux q' res
+        | Set.member x res   -> aux q' res
+        | otherwise          -> aux (foldr Set.insert q' (neighbors arr x)) (Set.insert x res)
 
-neighbors (x,y) = [(x+1,y),(x-1,y),(x,y-1),(x,y+1)]
+neighbors :: Array Coord (Maybe Piece) -> Coord -> [Coord]
+neighbors a (x,y) = filter (inArray a) [(x+1,y),(x-1,y),(x,y-1),(x,y+1)]
+
+-- | Return 'True' if the given index is in the given array.
+inArray :: Array Coord e -> Coord -> Bool
+inArray arr = inRange (bounds arr)
 
 -- * Drawing functions
 
-{-
-activeChar 0 = '▙'
-activeChar 1 = '▛'
-activeChar 2 = '▜'
-activeChar 3 = '▟'
-
-inactiveChar = '█'
--}
-
+activeChar :: Int -> Char
 activeChar 0 = '⬖'
-activeChar 1 = '⬘'
+activeChar 1 = '◆'
 activeChar 2 = '⬗'
-activeChar 3 = '⬙'
+activeChar 3 = '◇'
+
+animationLength :: Int
+animationLength = 4
+
+inactiveChar :: Char
 inactiveChar = '◆'
 
-data RedrawEvent
-  = TimerTick
-  | Refresh
+emptyChar :: Char
+emptyChar = ' '
 
-data Piece
-  = Red
-  | Purple
-  | Green
-  deriving (Eq, Show, Read)
 
+data RedrawEvent = TimerTick | Refresh
+
+data Piece = Red | Purple | Green
+  deriving (Eq)
+
+randomPiece :: IO Piece
 randomPiece = do
-  x <- randomRIO (0 :: Int, 2)
-  return $! case x of
+  x <- randomRIO (0, 2)
+  return $! case x :: Int of
     0 -> Red
     1 -> Purple
     2 -> Green
 
-colorToAttr Red    = with_fore_color def_attr red
-colorToAttr Green  = with_fore_color def_attr green
-colorToAttr Purple = with_fore_color def_attr magenta
+pieceToAttr :: Piece -> Attr
+pieceToAttr Red    = with_fore_color def_attr red
+pieceToAttr Green  = with_fore_color def_attr green
+pieceToAttr Purple = with_fore_color def_attr magenta
 
-colorToAttr' Red    = with_fore_color def_attr bright_red
-colorToAttr' Green  = with_fore_color def_attr bright_green
-colorToAttr' Purple = with_fore_color def_attr bright_magenta
-
+clock :: MVar RedrawEvent -> IO ()
 clock eventVar = forever $ do
-  threadDelay 250000
+  threadDelay animationDelay
   putMVar eventVar TimerTick
 
+toCursor :: Coord -> Cursor
+toCursor (r,c) = Cursor (fromIntegral c + 1) (fromIntegral r + 1)
+
+drawingThread :: Vty -> MVar RedrawEvent -> IORef GameState -> Int -> IO ()
 drawingThread vty eventVar gameVar ctr = do
   ev <- takeMVar eventVar
   st <- readIORef gameVar
-  let ctr' = (ctr + 1) `mod` 4
+  let ctr' = (ctr + 1) `mod` animationLength
   case ev of
     TimerTick -> update vty (draw ctr' st) >> drawingThread vty eventVar gameVar ctr'
     Refresh   -> update vty (draw ctr st) >> drawingThread vty eventVar gameVar ctr
 
-draw ctr st = Picture { pic_image = vert_cat [drawRow i | i <- [rowLo..rowHi]]
-                      , pic_cursor = Cursor (fromIntegral (snd (cursorCoord st))) (fromIntegral (fst (cursorCoord st)))
+draw ctr st = Picture { pic_image = image
+                      , pic_cursor = toCursor (cursorCoord st)
                       , pic_background = Background ' ' def_attr }
   where
+  image = boxImage boardImage <|> char def_attr ' ' <|> infoImage
+
+  infoImage = string def_attr "══╡ CONSOLE GAME ╞══"
+          <-> char def_attr ' '
+          <-> string def_attr "Score: "
+          <|> string (with_fore_color def_attr red) (show (totalScore st))
+          <-> char def_attr ' '
+          <-> lastScoreImage
+          <-> char def_attr ' '
+          <-> gameOverImage
+          <-> char def_attr ' '
+          <-> controlsImage
+
+  controlsImage = string def_attr "Controls: ←↑↓→ ␛ ⏎"
+              <-> string def_attr "New game: N"
+
+  lastScoreImage = case lastScore st of
+    Nothing -> char def_attr ' '
+    Just n  -> string def_attr ("Group worth " ++ show n)
+
+  gameOverImage
+    | gameOver st = string def_attr "Game Over!"
+    | otherwise   = char def_attr ' '
+
   activeSet = cachedConnectedSet st
 
-  drawRow r = horiz_cat [drawCell r i (boardArray st ! (r,i)) | i <- [colLo..colHi]]
-
-  drawCell _ _ Nothing = char def_attr ' '
-  drawCell r c (Just p) | Set.size activeSet > 1 && Set.member (r,c) activeSet = char (colorToAttr' p) (activeChar ctr)
-                        | otherwise              = char (colorToAttr p) inactiveChar
-
   ((rowLo, colLo),(rowHi,colHi)) = bounds (boardArray st)
+  boardImage = vert_cat  [drawRow r                            | r <- [rowLo..rowHi]]
+  drawRow r  = horiz_cat [drawCell r c (boardArray st ! (r,c)) | c <- [colLo..colHi]]
+
+  drawCell _ _ Nothing      = char def_attr        emptyChar
+  drawCell r c (Just p)
+    | inEligibleGroup (r,c) = char (pieceToAttr p) (activeChar ctr)
+    | otherwise             = char (pieceToAttr p) inactiveChar
+  
+  inEligibleGroup x = Set.size activeSet > 1 && Set.member x activeSet
+    
+boxImage :: Image -> Image
+boxImage img = c '┌' <|> hbar <|> c '┐'
+           <-> vbar  <|> img  <|> vbar
+           <-> c '└' <|> hbar <|> c '┘'
+  where
+  w = image_width img
+  h = image_height img
+  c = char def_attr
+  hbar = char_fill def_attr '─' w 1
+  vbar = char_fill def_attr '│' 1 h
